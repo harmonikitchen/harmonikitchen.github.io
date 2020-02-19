ff95422acab46ffe1bd5b52c544289afa66f89fe
andre <ndree.timothy@gmail.com>
2020-02-19 21:29:14 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var elements = document.getElementsByClassName("image");
+
+// Declare a loop variable
+var i;
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+});
+}
